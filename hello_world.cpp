2552f073f40887269ff1a7e741208b133d8729f4
Daniil <daniil.s20035@mail.ru>
2022-03-09 02:22:10 +0300

    Changes for the hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-cout << "Hello, World!" << endl;
+string name;
+cout << "Name: "; std::cin >> name;
+cout << "Hello, World from " << name << endl;
 }
