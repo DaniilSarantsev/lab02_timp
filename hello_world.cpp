4988cccdc6c9b25de6764623ba6eda625e1e4fa4
Daniil <91692911+DaniilSarantsev@users.noreply.github.com>
2022-03-09 08:35:22 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-std::string name; //name of user
+std::string name; //name of user!
 std::cout << "Name: "; std::cin >> name; //enter a name of user
 std::cout << "Hello, World from " << name << std::endl;
 }
