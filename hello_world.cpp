e90d56720d8d999ca95f5f068e6a4d554efb3dbe
Daniil <daniil.s20035@mail.ru>
2022-03-09 08:03:41 +0300

    Changes for the part III

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,11 @@
 
 using namespace std;
 
-int main() {
-string name;
-cout << "Name: "; std::cin >> name;
-cout << "Hello, World from " << name << endl;
+int
+main()
+{
+  string name;
+  cout << "Name: ";
+  std::cin >> name;
+  cout << "Hello, World from " << name << endl;
 }
