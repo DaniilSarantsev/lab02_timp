16905fd4295dfdfd99c98304d0336624dc9b8cf7
Daniil <daniil.s20035@mail.ru>
2022-03-09 08:21:50 +0300

    Fixed conflicts

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 
-<<<<<<< HEAD
 int
 main()
 {
@@ -9,10 +8,4 @@ main()
   cout << "Name: "; 
   std::cin >> name; //enter a name of user
   cout << "Hello, World from " << name << endl;
-=======
-int main() {
-std::string name; //name of user
-std::cout << "Name: "; std::cin >> name; //enter a name of user
-std::cout << "Hello, World from " << name << std::endl;
->>>>>>> 68a3072 (Changes for the file)
 }
